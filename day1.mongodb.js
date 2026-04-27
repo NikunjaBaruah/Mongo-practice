8c1fef932adda6a47cb7a86edb17b60f288e9a0b
NIKUNJA <96947369+NikunjaBaruah@users.noreply.github.com>
2026-04-27 21:43:51 +0530

    Add files via upload

---
+++ day1.mongodb.js
@@ -0,0 +1,25 @@
+
+
+db.products.insertMany([
+    { name: "Laptop", category: "Electronics", price: 55000, stock: 10, brand: "HP", rating: 4.5 },
+    { name: "Smartphone", category: "Electronics", price: 20000, stock: 25, brand: "Samsung", rating: 4.2 },
+    { name: "Headphones", category: "Electronics", price: 2000, stock: 50, brand: "Boat", rating: 4.0 },
+    { name: "Shoes", category: "Fashion", price: 3000, stock: 30, brand: "Nike", rating: 4.3 },
+    { name: "T-shirt", category: "Fashion", price: 800, stock: 100, brand: "Puma", rating: 3.9 },
+    { name: "Watch", category: "Accessories", price: 5000, stock: 20, brand: "Fossil", rating: 4.6 },
+    { name: "Backpack", category: "Accessories", price: 1500, stock: 40, brand: "Wildcraft", rating: 4.1 },
+    { name: "Tablet", category: "Electronics", price: 30000, stock: 15, brand: "Apple", rating: 4.7 },
+    { name: "Keyboard", category: "Electronics", price: 1200, stock: 60, brand: "Logitech", rating: 4.4 },
+    { name: "Mouse", category: "Electronics", price: 700, stock: 80, brand: "HP", rating: 4.2 },
+  
+    { name: "Jeans", category: "Fashion", price: 2000, stock: 35, brand: "Levis", rating: 4.3 },
+    { name: "Jacket", category: "Fashion", price: 4500, stock: 12, brand: "Zara", rating: 4.5 },
+    { name: "Sunglasses", category: "Accessories", price: 2500, stock: 18, brand: "RayBan", rating: 4.6 },
+    { name: "Charger", category: "Electronics", price: 900, stock: 70, brand: "Mi", rating: 4.0 },
+    { name: "Power Bank", category: "Electronics", price: 1800, stock: 45, brand: "Ambrane", rating: 4.1 },
+    { name: "Speaker", category: "Electronics", price: 3500, stock: 22, brand: "JBL", rating: 4.4 },
+    { name: "Monitor", category: "Electronics", price: 12000, stock: 14, brand: "Dell", rating: 4.5 },
+    { name: "Printer", category: "Electronics", price: 8000, stock: 9, brand: "Canon", rating: 4.2 },
+    { name: "Bag", category: "Fashion", price: 2500, stock: 28, brand: "Skybags", rating: 4.1 },
+    { name: "Cap", category: "Fashion", price: 500, stock: 90, brand: "Adidas", rating: 3.8 }
+  ])
